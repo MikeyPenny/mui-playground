6db060f4b965c68dddef6e6e6f6a4fc0d5d56c2a
Miguel Sandoval <miguelsandoval@Miguels-MacBook-Pro.local>
2022-05-24 16:03:38 +0200

    Add transition to the AppBar

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -6,6 +6,8 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -19,27 +21,43 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
-export default function Header() {
+function ElevationScroll(props) {
+	const { children } = props;
+
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0,
+	});
+
+	return React.cloneElement(children, {
+		elevation: trigger ? 4 : 0,
+	});
+}
+
+export default function Header(props) {
 	const classes = useStyles();
 
 	return (
-		<div className={classes.root}>
-			<AppBar position='fixed'>
-				<Toolbar>
-					<IconButton
-						edge='start'
-						className={classes.menuButton}
-						color='inherit'
-						aria-label='menu'
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant='h6' className={classes.title}>
-						News
-					</Typography>
-					<Button color='inherit'>Login</Button>
-				</Toolbar>
-			</AppBar>
-		</div>
+		<React.Fragment>
+			<CssBaseline />
+			<ElevationScroll {...props}>
+				<AppBar position='fixed'>
+					<Toolbar>
+						<IconButton
+							edge='start'
+							className={classes.menuButton}
+							color='inherit'
+							aria-label='menu'
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant='h6' className={classes.title}>
+							News
+						</Typography>
+						<Button color='inherit'>Login</Button>
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+		</React.Fragment>
 	);
 }
